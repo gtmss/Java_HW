509ee3196ddc454410884a1c7b0572f0f7ba380b
gtmss <ion.gatman1@isa.utm.md>
2022-03-09 00:48:56 +0200

    ex 26 finished

--- Main215/ch2/Ex_26_2.java
+++ Main215/ch2/Ex_26_2.java
@@ -1,2 +1,14 @@
+import java.util.Scanner;
+
 public class Ex_26_2 {
+    public static void main(String[] args) {
+        Scanner Input = new Scanner(System.in);
+
+        int num1 = Input.nextInt();
+        int num2 = Input.nextInt();
+
+        if ((num1 % num2) == 0) {
+            System.out.printf("%d is multiple of %d", num1, num2);
+        }
+    }
 }
