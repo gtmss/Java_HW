950fd8fdfc61248f904c819391e508d748312520
Ion Gataman <ion.gatman1@isa.utm.md>
2022-03-10 09:07:27 +0200

    ex 28 finished

---
+++ Main215/ch2/Ex_28_2.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+import static java.lang.Math.PI;
+import static java.lang.Math.pow;
+
+public class Ex_28_2 {
+    public static void main(String[] args) {
+        System.out.println("Introduce the radius of a circle:");
+        Scanner Input = new Scanner(System.in);
+
+        int radius = Input.nextInt();
+        double diameter = 2 * radius;
+        double circumference = 2 * PI * radius;
+        double area = PI * pow(radius, 2);
+
+        System.out.printf("Radius: %d\n", radius);
+        System.out.printf("Diameter: %f\n", diameter);
+        System.out.printf("Cricumference: %f\n", circumference);
+        System.out.printf("Area: %f\n", area);
+    }
+}
