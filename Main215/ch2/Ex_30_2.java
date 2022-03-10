3452d2ec21469e374daf0a7192b962dd2e53479f
gtmss <ion.gatman1@isa.utm.md>
2022-03-10 12:33:19 +0200

    ex 30 finished

---
+++ Main215/ch2/Ex_30_2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Ex_30_2 {
+    public static void main(String[] args) {
+        Scanner Input = new Scanner(System.in);
+
+        System.out.println("Eneter an integer number");
+        int num = Input.nextInt();
+        int mod;
+
+        while (num > 0){
+            mod = num % 10;
+            System.out.printf("%d ", mod);
+            num = num / 10;
+        }
+    }
+}
