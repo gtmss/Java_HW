61cce2c0a44abd5af1ebb0d3075a0c0e18694f6e
gtmss <ion.gatman1@isa.utm.md>
2022-03-15 23:45:59 +0200

    Ex 15 -> added validation

--- Main215/source/ch2/Ex_15_2.java
+++ Main215/source/ch2/Ex_15_2.java
@@ -6,22 +6,26 @@ public class Ex_15_2 {
     public static void main(String[] args) {
         Scanner Input = new Scanner(System.in);
 
-        int num1 = 0;
-        int num2 = 0;
 
         System.out.println("Introduce num1: ");
-        num1 = Input.nextInt();
+        int num1 = Input.nextInt();
         System.out.println("Introduce num2: ");
-        num2 = Input.nextInt();
+        int num2 = Input.nextInt();
         int sum = num1 + num2;
-        int product = num1 * num2;
         int dif = num1 - num2;
-        int quotient = num1 / num2;
 
         System.out.println();
         System.out.println("SUM: "+sum);
-        System.out.println("PRODUCT: "+product);
         System.out.println("DIFFERENCE: "+dif);
-        System.out.println("QUOTIENT: "+quotient);
+        if (num1 * num2 < Integer.MAX_VALUE){
+            long product = num1 * num2;
+            System.out.println("PRODUCT: "+product);
+        } else System.out.println("Error. Too large resutt !");
+
+        if (num2 != 0) {
+            int quotient = num1 / num2;
+            System.out.println("QUOTIENT: "+quotient);
+        } else System.out.println("Division by 0!");
+
     }
 }
