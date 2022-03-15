9524c3de35750056f84edab895dbfacb197ddeb2
gtmss <ion.gatman1@isa.utm.md>
2022-03-15 22:52:00 +0200

    Ex 17 - issue solved

--- Main215/source/ch2/Ex_17_2.java
+++ Main215/source/ch2/Ex_17_2.java
@@ -29,7 +29,7 @@ public class Ex_17_2 {
         } else if ((num2 > num1) && (num2 > num3)) {
             System.out.println("The largest is "+num2);
         } else if ((num3 > num2) && (num3 > num1)) {
-            System.out.println("The largest is "+num1);
+            System.out.println("The largest is "+num3);
         } else System.out.println("\nNumbers are equal");
     }
 }
