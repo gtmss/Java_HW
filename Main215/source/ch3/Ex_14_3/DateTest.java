72cf3b512233577db050a95af9ded09f627f1920
gtmss <ion.gatman1@isa.utm.md>
2022-03-22 15:54:38 +0200

    Ex 14_3 - finished

--- Main215/source/ch3/Ex_14_3/DateTest.java
+++ Main215/source/ch3/Ex_14_3/DateTest.java
@@ -6,8 +6,8 @@ public class DateTest {
         Date date2 = new Date(22, 4, 2012);
         Date date3 = new Date(9, 10, 2001);
 
-        date1.display(date1.getMonth(), date1.getDate(), date1.getYear());
-        date2.display(date2.getMonth(), date2.getDate(), date2.getYear());
-        date3.display(date3.getMonth(), date3.getDate(), date3.getYear());
+        date1.display(date1.getDate(), date1.getMonth(), date1.getYear());
+        date2.display(date2.getMonth(), date2.getMonth(), date2.getYear());
+        date3.display(date3.getMonth(), date3.getMonth(), date3.getYear());
     }
 }
