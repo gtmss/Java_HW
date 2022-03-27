a8641cda08d7a89d9d9d4230480d1187ce711b49
gtmss <ion.gatman1@isa.utm.md>
2022-03-27 16:10:18 +0300

    Ex 14_4 - finished

--- Main215/source/ch3/Ex_15_3/AccountTest.java
+++ Main215/source/ch3/Ex_15_3/AccountTest.java
@@ -3,21 +3,24 @@ package ch3.Ex_15_3;
 import java.util.Scanner;
 
 public class AccountTest {
-    Account account1 = new Account("Jane Green",50.00);
-    Account account2 = new Account("John Blue", -7.53);
+    public static void main(String[] args) {
 
-    displayAccount(account1);
-    displayAccount(account2);
 
-    Scanner input = new Scanner(System.in);
+        Account account1 = new Account("Jane Green", 50.00);
+        Account account2 = new Account("John Blue", -7.53);
+
+        displayAccount(account1);
+        displayAccount(account2);
+
+        Scanner input = new Scanner(System.in);
         System.out.print("Enter deposit amount for account1 : ");
-    double depositAmount = input.nextDouble();
+        double depositAmount = input.nextDouble();
 
         System.out.printf("%n adding %.2f to account1 balance %n%n", depositAmount);
         account1.deposit(depositAmount);
 
-    displayAccount(account1);
-    displayAccount(account2);
+        displayAccount(account1);
+        displayAccount(account2);
 
         System.out.print("Enter deposit amount for account2 : ");
         depositAmount = input.nextDouble();
@@ -25,12 +28,12 @@ public class AccountTest {
         System.out.printf("%n adding %.2f to account2 balance %n%n", depositAmount);
         account2.deposit(depositAmount);
 
-    displayAccount(account1);
-    displayAccount(account2);
+        displayAccount(account1);
+        displayAccount(account2);
 
-}
-    public static void displayAccount(Account accountToDisplay)
-    {
-        System.out.printf("%s balance : $%.2f%n",accountToDisplay.getName(),accountToDisplay.getBalance());
+    }
+
+    public static void displayAccount(Account accountToDisplay) {
+        System.out.printf("%s balance : $%.2f%n", accountToDisplay.getName(), accountToDisplay.getBalance());
     }
 }
---
+++ Main215/source/ch4/Ex_14_4.java
@@ -0,0 +1,44 @@
+package ch4;
+
+import java.util.Scanner;
+
+public class Ex_14_4 {
+
+    public static void main(String[] args) {
+        Scanner Input = new Scanner(System.in);
+
+        int gallons;
+        int totalGallons = 0;
+        int miles;
+        int totalMiles = 0;
+        int tripCounter = 0;
+        String check;
+
+        System.out.println("Fo you want to infroduce some noew data about your trips ?\n" +
+                "press y - YES or n - NO");
+        check = Input.next();
+
+        while (!check.equals("n")){
+            tripCounter++;
+            System.out.printf("Trip number: %d\n", tripCounter);
+            System.out.println("How much MILES ?");
+            miles = Input.nextInt();
+            System.out.println("How about GALLONS ?");
+            gallons = Input.nextInt();
+            System.out.printf("Fuel consumption: %.2f\n", (double)miles/gallons);
+            totalMiles += miles;
+            totalGallons += gallons;
+
+            System.out.println("Dou you want to add a new trip ? Press yES or nO");
+            check = Input.next();
+        }
+
+        //Combined consuption.
+
+        System.out.println("****************************************");
+        System.out.printf("Total miles: %d\n", totalMiles);
+        System.out.printf("Total gallons: %d\n", totalGallons);
+        System.out.printf("Combined fuel consumption: %.2f miles/gallons\n", (double)totalMiles/totalGallons);
+    }
+
+}
